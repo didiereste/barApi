976ba8b8a8860c5c29e889181edf421dce088a17
Didier <didiereste@gmail.com>
2025-01-12 11:18:23 -0500

    Instalar CORS

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const cors = require('cors');
 const jwt = require("jsonwebtoken");
 const authRoutes = require("./src/routes/authRoutes");
 const roleRoutes = require("./src/routes/roleRoutes");
 const userRoutes = require("./src/routes/userRoutes");
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true 
+}));
+
 app.use(express.json());
 
 app.use('/barapp/v1/auth', authRoutes);
